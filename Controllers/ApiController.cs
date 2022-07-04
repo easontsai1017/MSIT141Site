bba1509aeb3dc7325641543843cbe74caaed6702
eason.tsai1017@gmail.com <eason.tsai1017@gmail.com>
2022-07-04 19:43:06 +0800

    作業7/4

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -9,14 +9,36 @@ namespace MSIT141Site.Controllers
 {
     public class ApiController : Controller
     {
+        private DemoContext _context;
+        public ApiController(DemoContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index(User user)
         {
             //return Content("Ajax,您好","text/plain",System.Text.Encoding.UTF8);
-            if(string.IsNullOrEmpty(user.name))
+            if (string.IsNullOrEmpty(user.name))
+                return Content("請輸入姓名", "text/plain", System.Text.Encoding.UTF8);
+            else
             {
-                user.name = "Ajax";
+                Member mem = _context.Members.FirstOrDefault(m => m.Name == user.name);
+                if (mem != null)
+                    return Content("已有此名稱，請重新輸入", "text/plain", System.Text.Encoding.UTF8);
+                else
+                    return Content("可使用此名稱", "text/plain", System.Text.Encoding.UTF8);
             }
-            return Content($"{user.name}您好,你的年紀是{user.age}", "text/plain", System.Text.Encoding.UTF8);
+            //return Content($"{user.name}您好,你的年紀是{user.age},信箱帳號{user.email}", "text/plain", System.Text.Encoding.UTF8);
         }
+
+        //public IActionResult CheckName(User user)
+        //{
+
+        //    if(user.name)
+        //}
+
+
+
+
+
     }
 }
