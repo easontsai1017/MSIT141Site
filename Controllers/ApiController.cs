7b052d04e7d9e6a70318404dda37f9e2089b0a70
eason.tsai1017@gmail.com <eason.tsai1017@gmail.com>
2022-07-04 11:51:00 +0800

    新增name&age用法

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -8,9 +8,14 @@ namespace MSIT141Site.Controllers
 {
     public class ApiController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string name,int age=0)
         {
-            return Content("Ajax,您好","text/plain",System.Text.Encoding.UTF8);
+            //return Content("Ajax,您好","text/plain",System.Text.Encoding.UTF8);
+            if(string.IsNullOrEmpty(name))
+            {
+                name = "Ajax";
+            }
+            return Content($"{name}您好,你的年紀是{age}", "text/plain", System.Text.Encoding.UTF8);
         }
     }
 }
