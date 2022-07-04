85e70bbec1473bda7746eb044b116c8d55fb6b9f
eason.tsai1017@gmail.com <eason.tsai1017@gmail.com>
2022-07-04 17:37:31 +0800

    更新作業模組

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MSIT141Site.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +9,14 @@ namespace MSIT141Site.Controllers
 {
     public class ApiController : Controller
     {
-        public IActionResult Index(string name,int age=0)
+        public IActionResult Index(User user)
         {
             //return Content("Ajax,您好","text/plain",System.Text.Encoding.UTF8);
-            if(string.IsNullOrEmpty(name))
+            if(string.IsNullOrEmpty(user.name))
             {
-                name = "Ajax";
+                user.name = "Ajax";
             }
-            return Content($"{name}您好,你的年紀是{age}", "text/plain", System.Text.Encoding.UTF8);
+            return Content($"{user.name}您好,你的年紀是{user.age}", "text/plain", System.Text.Encoding.UTF8);
         }
     }
 }
